49088a59db9986774188c23fe4107ca41b17c329
Arnab Dhara <adhara@utexas.edu>
2023-02-21 11:18:40 -0600

    fwi22

--- trainVelAutoElFullRho4JanModelPhyInitial.sh
+++ trainVelAutoElFullRho4JanModelPhyInitial.sh
@@ -1,4 +1,4 @@
 rm -rf ./marmousiEl4JanInit
 mkdir ./marmousiEl4JanInit
 rm -rf /disk/student/adhara/Fall2021/PhysicsBasedFWI/checkpoints/MarmousiElFull4JanInit/*.txt
-no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiElInit/ --name MarmousiElFull4JanInit --model AutoElFullRhoScaleMar22 --direction AtoB --input_nc 46 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2500 --batch_size 8 --gpu_ids 3 --no_html --display_freq 1 --print_freq 1 --lr 0.002 --verbose --save_epoch_freq 18 --init_type kaiming --epoch 288 --continue_train
+no_proxy=localhost python trainValLatent4dVel2Elastic.py --dataroot /disk/student/adhara/Fall2021/FCNVMB-Deep-learning-based-seismic-velocity-model-building/MarmousiElInit/ --name MarmousiElFull4JanInit --model AutoElFullRhoScaleMar22 --direction AtoB --input_nc 46 --output_nc 1 --display_port 9998 --n_epochs 4000  --n_epochs_decay 2500 --batch_size 8 --gpu_ids 3 --no_html --display_freq 1 --print_freq 1 --lr 0.002 --verbose --save_epoch_freq 18 --init_type kaiming --epoch 432 --continue_train
