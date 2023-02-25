598d508e548b22c19d95a4e0f60e2e5a2781f4f2
Arnab Dhara <adhara@utexas.edu>
2023-02-25 00:36:06 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -2537,7 +2537,7 @@ class unetConv10(nn.Module):
         super(unetConv10, self).__init__()
         # Kernel size: 3*3, Stride: 1, Padding: 1
         if is_batchnorm:
-            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, (3, 5), (1,1), (1,2)),
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, (5, 3), (1,1), (2,1)),
                                        nn.BatchNorm2d(out_size),
                                        nn.LeakyReLU(0.1))
             #self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1),
