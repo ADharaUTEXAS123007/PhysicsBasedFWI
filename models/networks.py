910fb5b114f0aead3ddc0ef21473364b3d80f26d
Arnab Dhara <adhara@utexas.edu>
2023-02-26 12:26:13 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -10114,7 +10114,7 @@ class AutoElBPRhoScaleMarmousiMar22_Net(nn.Module):
         ##self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input1 = nn.Linear(filters[2]*125*26, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[2]*500*102) #for marmousi 151x200
-        self.decoder_input1 = nn.Linear(filters[3]*63*24, 8) #for marmousi 101x101
+        self.decoder_input1 = nn.Linear(filters[3]*73*13, 8) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
         self.decoder_input = nn.Linear(8, filters[3]*52*23) #for marmousi 101x101
