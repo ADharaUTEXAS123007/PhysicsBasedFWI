3bf13c8abfd01d4525d7dbb09d08ff80d701e759
Arnab Dhara <adhara@utexas.edu>
2023-02-26 12:28:19 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -10117,7 +10117,7 @@ class AutoElBPRhoScaleMarmousiMar22_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*73*13, 8) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(8, filters[3]*52*23) #for marmousi 101x101
+        self.decoder_input = nn.Linear(8, filters[3]*52*30) #for marmousi 101x101
         #############self.decoder_input2 = nn.Linear(latent_dim, 4*92*208)
         #self.decoder_inputRho = nn.Linear(latent_dim, 1*300*100)
         
@@ -10267,8 +10267,8 @@ class AutoElBPRhoScaleMarmousiMar22_Net(nn.Module):
         #####z = inputs2
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
         #print("shape of z :", np.shape(z))
-        z = z.view(-1, filters[3], 23, 52)
-        z1 = z.view(-1,filters[3],23,52)
+        z = z.view(-1, filters[3], 30, 52)
+        z1 = z.view(-1,filters[3],30,52)
         #z2 = z2.view(-1, 4, 92, 208)
         #zrho = zrho.view(-1, 1, 100, 300)
         #down4 = torch.swapaxes(down4,2,3)
