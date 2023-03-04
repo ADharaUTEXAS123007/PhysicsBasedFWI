1ea5dacd562094baf467979bb8ba39c5b00728cb
Arnab Dhara <adhara@utexas.edu>
2023-03-03 22:41:25 -0600

    fwi

--- models/networks.py
+++ models/networks.py
@@ -9019,7 +9019,7 @@ class AutoElFullRhoMarmousiMar22_Net(nn.Module):
 
         vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1f
         vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1f
-        rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + 0.1*(0.7*rho1f + 0.3*(0.304*dind+194.6))
+        rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + 0.1*(0.6*rho1f + 0.4*(0.304*dind+194.6))
 
         #################4################# print("before rho1 norm :", torch.norm(torch.unsqueeze(lowf[:,2,:,:],1)))
         #rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + 0.0005*rho1f
