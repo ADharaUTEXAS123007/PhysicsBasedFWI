fa8ad5afe96f4124b9d5620c3c222b0bcebca53b
Arnab Dhara <adhara@utexas.edu>
2023-03-09 17:29:14 -0600

    initial

--- models/networks.py
+++ models/networks.py
@@ -14852,7 +14852,7 @@ class AutoMarmousiWav_Net(nn.Module):
                     lossinner1 = criterion2(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
                     #lossinner2 = criterion2(batch_rcv_amps_pred_norm, batch_rcv_amps_true)
                     print("batch src amps :", np.shape(batch_src_amps))
-                    lossinner = lossinner1 + 10*torch.norm(torch.diff(batch_src_amps,dim=0))
+                    lossinner = lossinner1 + torch.norm(torch.diff(batch_src_amps,dim=0))
                     ###lossinner = lossinner1
                     #y_c_features = vgg(torch.unsqueeze(batch_rcv_amps_true,0))
                     #########model2.grad[0:26,:] = 0
