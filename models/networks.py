83adb431a3090f4e789000ad117e54c5f13f66f4
Arnab Dhara <adhara@utexas.edu>
2023-03-31 21:25:31 -0500

    loss_land

--- models/networks.py
+++ models/networks.py
@@ -6751,7 +6751,7 @@ class AutoElLinear22_Net(nn.Module):
         vp1    = torch.unsqueeze(lowf[:,0,:,:],1) + vp1
         vs1    = torch.unsqueeze(lowf[:,1,:,:],1) + vs1
         #rho1   = torch.unsqueeze(lowf[:,2,:,:],1) + 0.1*rho1
-        rho1   = torch.unsqueeze(inputs2[:,2,:,:],1)
+        rho1   = torch.unsqueeze(inputs1[:,2,:,:],1)
     
         
         #vp1     = self.final1(vp1)
