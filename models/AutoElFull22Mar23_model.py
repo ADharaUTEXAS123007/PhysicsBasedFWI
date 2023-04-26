7b7bde61c19c77e2dcdf90f4a6454d236f7b74cf
Arnab Dhara <adhara@utexas.edu>
2023-04-26 15:43:06 -0500

    fwi

--- models/AutoElFull22Mar23_model.py
+++ models/AutoElFull22Mar23_model.py
@@ -511,8 +511,8 @@ class AutoElFull22Mar23Model(BaseModel):
 
     def optimize_parameters(self, epoch, batch, lstart, freq, initerror, currenterror):
         num_shots = 90
-        idx = np.random.permutation(num_shots)
-        #idx = np.arange(0,num_shots)
+        #idx = np.random.permutation(num_shots)
+        idx = np.arange(0,num_shots)
         num_batches = 1
         for it in range(num_batches):
             self.forward(epoch,lstart,freq,idx,it)                   # compute fake images: G(A)
