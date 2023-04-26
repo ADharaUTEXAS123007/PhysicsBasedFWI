ad3c5706100f5e24293a07c24fbd5be114fd7a71
Arnab Dhara <adhara@utexas.edu>
2023-04-26 17:40:34 -0500

    fwi

--- models/networks.py
+++ models/networks.py
@@ -9760,7 +9760,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         
         #print("idx idx idx :", len(idx))
         print("epoch1 :", epoch1)
-        idx = idx[0:30]
+        idx = idx[0:20]
         #if (epoch1%3 == 0):
         #    idx = idx[0:51:3]
         #elif (epoch1%3 == 1):
