486b6313fadcb47c4f40f799a80f404b05cb3957
Arnab Dhara <adhara@utexas.edu>
2023-04-25 19:46:40 -0500

    fwi

--- models/AutoElFull22Mar23_model.py
+++ models/AutoElFull22Mar23_model.py
@@ -239,7 +239,7 @@ class AutoElFull22Mar23Model(BaseModel):
         #netin1 = self.real_A[:, :, 1:800:2, :]
         #if (epoch1 == 1):
         self.latentT = torch.ones(1,1,1,1)
-        num_shots = 100
+        num_shots = 90
         idx = torch.arange(num_shots)
         [self.VpT,self.VsT,self.RhoT,self.gradT,self.latentT,_,_,_,_] = self.netG(self.real_B,self.real_A,False_lstart,False_epoch,self.latentT,self.real_C,self.real_D,freq,idx,1)  # G(A)
         #self.fake_BT = torch.clamp(self.fake_BT,min=15.00,max=35.50)
@@ -510,7 +510,7 @@ class AutoElFull22Mar23Model(BaseModel):
 
 
     def optimize_parameters(self, epoch, batch, lstart, freq, initerror, currenterror):
-        num_shots = 100
+        num_shots = 90
         #idx = np.random.permutation(num_shots)
         idx = np.arange(0,num_shots)
         num_batches = 1
