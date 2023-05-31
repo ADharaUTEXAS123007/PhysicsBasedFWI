0c47daa8c343b1a07887423395114e42e40502b4
Arnab Dhara <adhara@utexas.edu>
2023-05-31 10:05:37 -0500

    po

--- models/NewU_model.py
+++ models/NewU_model.py
@@ -106,7 +106,7 @@ class NewUModel(BaseModel):
 
     def forwardT(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
-        netin1 = self.real_A[:,:,1:2000:5,:]
+        netin1 = self.real_A[:,:,:]
         self.fake_BT = self.netG(netin1)  # G(A)
         self.real_BT = self.real_B
 
