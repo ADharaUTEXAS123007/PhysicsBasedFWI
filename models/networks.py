a92c1a89d66a912727293456665559a85b542647
Arnab Dhara <adhara@utexas.edu>
2023-07-15 16:08:30 -0500

    phy

--- models/networks.py
+++ models/networks.py
@@ -9615,7 +9615,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         ##self.decoder_input1 = nn.Linear(filters[1]*250*51, latent_dim) #for marmousi 151x200
         #self.decoder_input1 = nn.Linear(filters[2]*125*26, latent_dim) #for marmousi 151x200
         #self.decoder_input = nn.Linear(latent_dim, filters[2]*500*102) #for marmousi 151x200
-        self.decoder_input1 = nn.Linear(filters[3]*113*6, latent_dim) #for marmousi 101x101
+        self.decoder_input1 = nn.Linear(filters[3]*187*20, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
         self.decoder_input = nn.Linear(latent_dim, filters[3]*300*22) #for marmousi 101x101
@@ -9666,12 +9666,12 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         #meandata = torch.mean(inputs2)
         #stddata = torch.std(inputs2)
         ############################################################
-        combine1 = self.combine1((inputs2[:,0:90,1:1801,:]))
+        ##combine1 = self.combine1((inputs2[:,0:90,1:1801,:]))
         #combine2 = self.combine2((inputs3[:,:,1:1801,:]))
 
         #c1c2 = torch.cat((combine1,combine2),axis=1)
-        print("shape of inputs2 :", np.shape(inputs2[:,0:90,1:1801,:]))
-        print("shape of inputs1 :", np.shape(inputs1))
+        ##print("shape of inputs2 :", np.shape(inputs2[:,0:90,1:1801,:]))
+        ##print("shape of inputs1 :", np.shape(inputs1))
         #down1  = self.down1((inputs2[:,:,1:1200:4,:]))
         down1  = self.down1(inputs2)
         #down1  = self.dropD1(down1)
