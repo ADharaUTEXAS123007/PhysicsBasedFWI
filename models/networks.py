3a0b63e059cc7d457bd45213cfe60e88ed3a48e0
Arnab Dhara <adhara@utexas.edu>
2023-07-15 16:18:04 -0500

    phy

--- models/networks.py
+++ models/networks.py
@@ -9618,7 +9618,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         self.decoder_input1 = nn.Linear(filters[3]*187*20, latent_dim) #for marmousi 101x101
         #self.decoder_input = nn.Linear(latent_dim, filters[3]*100*26) #for marmousi 101x101
         #self.decoder_input1 = nn.Linear(filters[1]*100*18, latent_dim) #for marmousi 101x101
-        self.decoder_input = nn.Linear(latent_dim, filters[3]*400*35) #for marmousi 101x101
+        self.decoder_input = nn.Linear(latent_dim, filters[3]*400*50) #for marmousi 101x101
         #self.decoder_inputRho = nn.Linear(latent_dim, 1*300*100)
         
         
@@ -9698,7 +9698,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         #####z = inputs2
         #z = z.view(-1, filters[3], 250, 51) #for marmousi model
         #print("shape of z :", np.shape(z))
-        z = z.view(-1, filters[3], 35, 400)
+        z = z.view(-1, filters[3], 50, 400)
         #zrho = zrho.view(-1, 1, 100, 300)
         print("shape of inputs2 :", np.shape(inputs2))
     
