6dda4bdde2f33194f4bc246075673f886c8afffe
Arnab Dhara <adhara@utexas.edu>
2023-08-08 23:52:56 -0500

    p

--- models/networks.py
+++ models/networks.py
@@ -9593,7 +9593,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         #filters = [16, 32, 64, 128, 512]
         #######filters = [2, 4, 8, 16, 32] #this works best result so far for marmousi model
         #filters = [1, 1, 2, 4, 16]
-        filters = [2, 4, 8, 16, 32] 
+        filters = [2, 2, 2, 2, 2] 
         #filters = [4,8,16,32,64]
         #filters = [4, 8, 16, 32, 64]
         #filters = [16, 32, 64, 128, 256]
@@ -9644,7 +9644,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         #filters = [2, 1, 1, 1, 1]
         #filters = [32, 64, 128, 256, 512]
         #filters = [4,8,16,32,64]
-        filters = [2, 4, 8, 16, 32]  ###this works very well
+        filters = [2, 2, 2, 2, 2]  ###this works very well
         #filters = [1, 1, 2, 4, 16]
         #filters = [16, 32, 64, 128, 256]
         #filters = [4, 8, 16, 32, 64]
