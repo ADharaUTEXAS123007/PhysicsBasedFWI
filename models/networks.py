4887dd97bce489bd7cce39f9309258b0f465e34c
Arnab Dhara <adhara@utexas.edu>
2023-08-10 13:46:52 -0500

    p

--- models/networks.py
+++ models/networks.py
@@ -9721,6 +9721,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         vp_grad = vp1*0
         vs_grad = vp1*0
         rho_grad = vp1*0
+        lossT = lossT*0
         
         #vs1 = vp1*0
         #rho1 = vp1*0
