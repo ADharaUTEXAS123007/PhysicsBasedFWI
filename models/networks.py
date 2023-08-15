8a78f5754e7560d9303e35c42c69728d30d31d9c
Arnab Dhara <adhara@utexas.edu>
2023-08-15 13:47:01 -0500

    po

--- models/networks.py
+++ models/networks.py
@@ -10010,7 +10010,7 @@ class AutoElFullMarmousi23Mar22_Net(nn.Module):
         #d.add_fwi_stage(fc_low=0.0, fc_high=int(epoch1/10)+1.0)
         #d.add_fwi_stage(fc_low=0.0, fc_high=30.0)
         print("freq freq freq :", freq)
-        d.add_fwi_stage(fc_low=5, fc_high=freq, inv_rho_iter=10000, spatfilter=4, wd_damp=2, wd_damp1=4)
+        d.add_fwi_stage(fc_low=5, fc_high=freq, inv_rho_iter=10000, spatfilter=4, wd_damp=3, wd_damp1=5)
 
         print(f'Stage {0}:\n\t{d.fwi_stages[0]}\n')
             
